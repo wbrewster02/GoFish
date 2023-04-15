6a770d194344d6446c33864b3f1f9e7954f4fe13
wbrewster02 <wbrewster02@gmail.com>
2023-04-15 17:51:29 -0400

    finished

--- Game.js
+++ Game.js
@@ -63,58 +63,148 @@ class Deck {
     }
 }
 const dealer = new Deck();
-dealer.deck.shuffleDeck();
-// console.log(dealer)
+dealer.shuffleDeck();
 
-//below is used to randomly select index from array (Deck) and add to hand1/hand2
-// var tmp = [];
-// while (team1.length < 4) {
-//   tmp.push(players.splice(Math.floor(Math.random() * players.length - 1), 1)[0]);
-//   team1.push(tmp.pop());
-// }
-
-// while (team2.length < 4) {
-//   tmp.push(players.splice(Math.floor(Math.random() * players.length - 1), 1)[0]);
-//   team2.push(tmp.pop());
-// }
 
 const hand1 = [];
 const hand2 = [];
 const tmp = [];
+const myHand = document.querySelector(".hd1 > .cards")
+const otherHand = document.querySelector(".hd2 > .cards")
+const request = document.querySelector("#request")
+
 function deal() {
     while (hand1.length < 7) {
-    tmp.push(dealer.deck.splice(Math.floor(Math.random() * dealer.deck.length - 1), 1)[0]);
-    hand1.push(tmp.pop());
-};
-while (hand2.length < 7) {
-    tmp.push(dealer.deck.splice(Math.floor(Math.random() * dealer.deck.length - 1), 1)[0]);
-    hand2.push(tmp.pop());
+        tmp.push(dealer.deck.splice(Math.floor(Math.random() * dealer.deck.length - 1), 1)[0]);
+        let card = tmp.pop()
+        hand1.push(card);
+        let fileName = `${card.values.toLowerCase()}_of_${card.suit.toLowerCase()}.png`
+        let image = document.createElement("img")
+        image.src = "assets/" + fileName
+        image.setAttribute('value', card.values)
+        image.setAttribute('suit', card.suit)
+        addToHand(myHand, image)
+        if (!request.querySelector(`[value="${card.values}"]`)) {
+            let option = document.createElement('option')
+            option.value = card.values
+            option.innerHTML = card.values
+            request.appendChild(option)
+        }
+    };
+
+    while (hand2.length < 7) {
+        tmp.push(dealer.deck.splice(Math.floor(Math.random() * dealer.deck.length - 1), 1)[0]);
+        let card = tmp.pop()
+        hand2.push(card);
+        let fileName = `cardBack.png`
+        let image = document.createElement("img")
+        image.src = "assets/" + fileName
+        image.setAttribute('value', card.values)
+        image.setAttribute('suit', card.suit)
+        otherHand.appendChild(image)
+        check4Set(card.values, otherHand)
+    }
 }
-}
 
-// // Obtain the img element, and assign it to the image variable
-// let image = document.getElementById('pet')
-
-// // Obtain the 'shrink-grow' button, and assign it to the changeSize variable
-// let changeSize = document.getElementById('shrink-grow')
-
-// //DO NOT CHANGE THE FOLLOWING CODE
-// changeSize.addEventListener("click", function() {
-//   if (image.style.height == "100px") {
-//     image.style.height = "25px"
-//   }
-//   else {
-//     image.style.height = "100px"
-//   }
-// })
 const btn = document.getElementById("btn")
-btn.addEventListener("click", dealer.deck.shuffleDeck())
+btn.addEventListener("click", () => {
+    dealer.shuffleDeck()
+    deal()
+})
 
 
-// const drawPile = []
+const ask = document.getElementById("ask")
+ask.addEventListener("click", () => {
+    let value = request.value;
+    let cards = otherHand.querySelectorAll(`[value="${value}"]`)
+    if (cards.length) {
+        cards.forEach(card => {
+            let suit = card.getAttribute('suit');
+            let fileName = `${value.toLowerCase()}_of_${suit.toLowerCase()}.png`
+            card.src = "assets/" + fileName
+            addToHand(myHand, card)
+            setTimeout(player2Turn, 1000)
+        })
+    } else {
+        alert("Go Fish!")
+        drawBtn.removeAttribute('disabled')
+    }
 
-//request card function needed
-//'go fish' function needed
-//add function needed
-//if 'value'='value' -> remove from x add to y
-//else console.log("go fish")
+})
+
+
+function player2Turn() {
+    let index = Math.ceil(Math.random() * otherHand.querySelectorAll('img').length)
+    let player2card = document.querySelector(`.hd2 img:nth-child(${index})`)
+    let value = player2card.getAttribute("value")
+    alert(`Do you have any ${value}s?`)
+    let cards = myHand.querySelectorAll(`[value="${value}"]`)
+    if (cards.length) {
+        cards.forEach(card => {
+            card.src = "assets/cardBack.png"
+            otherHand.appendChild(card)
+        })
+    } else {
+        alert("Go Fish!")
+        drawCard(otherHand)
+    }
+
+}
+
+
+const drawBtn = document.getElementById("drawPile")
+drawBtn.addEventListener("click", () => {
+    drawBtn.setAttribute('disabled', true)
+    drawCard(myHand)
+    setTimeout(player2Turn, 1000)
+})
+
+function drawCard(hand) {
+    let card = dealer.deck.pop();
+    if (hand == myHand) {
+        var fileName = `${card.values.toLowerCase()}_of_${card.suit.toLowerCase()}.png`
+    } else {
+        var fileName = "cardBack.png"
+    }
+    let image = document.createElement("img")
+    image.src = "assets/" + fileName
+    image.setAttribute('value', card.values)
+    image.setAttribute('suit', card.suit)
+    
+    addToHand(hand, image)
+}
+
+function addToHand(hand, image) {
+    let value = image.getAttribute('value')
+    let sameValue = hand.querySelector(`[value="${value}"]`)
+    if (sameValue) {
+        hand.insertBefore(image, sameValue)
+    } else {
+        hand.appendChild(image)
+    }
+
+    check4Set(value, hand)
+}
+
+function check4Set(value, hand) {
+    let cards = hand.querySelectorAll(`img[value="${value}"]`)
+    if (cards.length == 4) {
+        cards.forEach(card => card.remove())
+        let scoreBoard = hand.previousElementSibling.querySelector("span")
+        let score = parseInt(scoreBoard.innerHTML)
+        let newScore = score + 1;
+        scoreBoard.innerHTML = newScore
+
+        if (newScore == 7) {
+            if (hand == myHand) {
+                alert("Game Over! You Win!") 
+            }
+            else {
+                alert("Game Over! You Lose!")
+            }
+            location.reload()
+        }
+
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+// console.log(dealer)
+
+//below is used to randomly select index from array (Deck) and add to hand1/hand2
+// var tmp = [];
+// while (team1.length < 4) {
+//   tmp.push(players.splice(Math.floor(Math.random() * players.length - 1), 1)[0]);
+//   team1.push(tmp.pop());
+// }
+
+// while (team2.length < 4) {
+//   tmp.push(players.splice(Math.floor(Math.random() * players.length - 1), 1)[0]);
+//   team2.push(tmp.pop());
+// }
